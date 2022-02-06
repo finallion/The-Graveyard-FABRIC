b8397d9dff7e7a2d7493ab1166cc17dd96539542
Finallion <74151350+finallion@users.noreply.github.com>
2022-02-06 16:31:06 +0100

    Added flee goals for villagers against graveyard mobs. Updated version

---
+++ src/main/java/com/finallion/graveyard/mixin/VillagerEntityMixin.java
@@ -0,0 +1,30 @@
+package com.finallion.graveyard.mixin;
+
+import com.finallion.graveyard.entities.AcolyteEntity;
+import com.finallion.graveyard.entities.horde.GraveyardHordeEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.village.VillagerType;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VillagerEntity.class)
+public abstract class VillagerEntityMixin extends PassiveEntity {
+
+    public VillagerEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;Lnet/minecraft/village/VillagerType;)V", at = @At(value = "TAIL"))
+    private void inject(EntityType<? extends VillagerEntity> entityType, World world, VillagerType type, CallbackInfo ci) {
+        this.goalSelector.add(1, new FleeEntityGoal<>(this, GraveyardHordeEntity.class, 10.0F, 0.7D, 1.0D));
+        this.goalSelector.add(1, new FleeEntityGoal<>(this, AcolyteEntity.class, 10.0F, 0.7D, 1.0D));
+    }
+}
---
+++ src/main/java/com/finallion/graveyard/mixin/WanderingTraderEntityMixin.java
@@ -0,0 +1,27 @@
+package com.finallion.graveyard.mixin;
+
+import com.finallion.graveyard.entities.AcolyteEntity;
+import com.finallion.graveyard.entities.horde.GraveyardHordeEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ai.goal.FleeEntityGoal;
+import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WanderingTraderEntity.class)
+public abstract class WanderingTraderEntityMixin extends MerchantEntity {
+
+    public WanderingTraderEntityMixin(EntityType<? extends MerchantEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "initGoals", at = @At(value = "TAIL"))
+    private void initCustomGoal(CallbackInfo ci) {
+        this.goalSelector.add(1, new FleeEntityGoal<>(this, GraveyardHordeEntity.class, 10.0F, 0.5D, 0.5D));
+        this.goalSelector.add(1, new FleeEntityGoal<>(this, AcolyteEntity.class, 10.0F, 0.5D, 0.5D));
+    }
+}
